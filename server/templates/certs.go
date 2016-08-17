82680f5b8cb6457275f18d60e1af017101a79b64
Niall Sheridan <nsheridan@gmail.com>
2016-08-17 20:09:58 +0100

    Switch from bootstrap to skeleton

--- server/templates/certs.go
+++ server/templates/certs.go
@@ -4,67 +4,51 @@ package templates
 const Certs = `
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Issued Certificates</title>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Issued Certificates</title>
 
-    <!-- Bootstrap -->
-    <link href="/static/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="/static/css/normalize.css">
+	<link rel="stylesheet" href="/static/css/skeleton.css">
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+</head>
+<body>
+	<div class="container">
+		<div class="page-header">
+			<h1>Issued SSH Certificates</h1>
+		</div>
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-  <div class="container">
-  <div class="page-header">
-  <h1>Issued SSH Certificates</h1>
-  </div>
-
-  <form action="/admin/revoke" method="post" id="form_revoke">
-  {{ .CSRF }}
-  <table class="table table-hover table-condensed">
-	<tr>
-		<th>ID</th>
-		<th>Created</th>
-		<th>Expires</th>
-		<th>Principals</th>
-		<th>Revoked</th>
-		<th>Revoke</th>
-	</tr>
-
-  {{range .Certs}}
-	<div class="checkbox">
-	<tr>
-	<td>{{.KeyID}}</td>
-	<td>{{.CreatedAt}}</td>
-	<td>{{.Expires}}</td>
-	<td>{{.Principals}}</td>
-	<td>{{.Revoked}}</td>
-	<td>
-		{{if not .Revoked}}
-		<input type="checkbox" value="{{.KeyID}}" name="cert_id" id="cert_id" />
-		{{end}}
-	</td>
-	</tr>
+		<form action="/admin/revoke" method="post" id="form_revoke">
+		{{ .CSRF }}
+		<table>
+			<thead>
+			<tr>
+				<th>ID</th>
+				<th>Created</th>
+				<th>Expires</th>
+				<th>Principals</th>
+				<th>Revoked</th>
+				<th>Revoke</th>
+			</tr>
+			</thead>
+			<tbody>
+			{{range .Certs}}
+				<tr>
+				<td>{{.KeyID}}</td>
+				<td>{{.CreatedAt}}</td>
+				<td>{{.Expires}}</td>
+				<td>{{.Principals}}</td>
+				<td>{{.Revoked}}</td>
+				<td>{{if not .Revoked}}<input style="margin:0;" type="checkbox" value="{{.KeyID}}" name="cert_id" id="cert_id" />{{end}}</td>
+				</tr>
+			{{ end }}
+			</tbody>
+		</table>
+		</form>
+		<button class="button-primary" type="submit" form="form_revoke" value="Submit">Submit</button>
 	</div>
-  {{ end }}
-  </table>
-  </form>
-  <button class="btn btn-primary" type="submit" form="form_revoke" value="Submit">Submit</button>
-  </div>
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="/static/js/bootstrap.min.js"></script>
-  </body>
+</body>
 </html>
 
 `
--- server/templates/token.go
+++ server/templates/token.go
@@ -4,53 +4,43 @@ package templates
 const Token = `
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Token</title>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Token</title>
 
-    <!-- Bootstrap -->
-    <link href="/static/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <style>
-      <!--
-      .code {
-        border: none;
-        font-family: monospace;
-        font-weight: bold;
-        height: auto;
-        margin: 12px 12px 12px 12px;
-        padding: 24px 12px 12px 12px;
-        resize: none;
-        text-align: center;
-      }
-      -->
+	<link rel="stylesheet" href="/static/css/normalize.css">
+	<link rel="stylesheet" href="/static/css/skeleton.css">
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Source+Code+Pro" rel="stylesheet">
+	<style>
+	<!--
+	.code {
+		background-color: #eee;
+		border: solid 1px #ccc;
+		font-family: 'Source Code Pro', monospace;
+		font-weight: bold;
+		height: auto;
+		margin: 12px 12px 12px 12px;
+		padding: 24px 12px 12px 12px;
+		resize: none;
+		text-align: center;
+	}
+	-->
 	</style>
-  </head>
-  <body>
-  <div class="container">
-  <div class="page-header">
-  <h1>Access Token</h1>
-  </div>
-	<div>
-	<textarea style="font-size: 15pt" class="form-control code" readonly spellcheck="false" onclick="this.focus();this.select();">{{.Token}}</textarea>
-    <h2>
-      The token will expire in &lt; 1 hour.
-    </h2>
+</head>
+<body>
+	<div class="container">
+		<div class="page-header">
+			<h1>Access Token</h1>
+		</div>
+		<div>
+			<textarea style="font-size: 15pt" class="u-full-width code" readonly spellcheck="false" onclick="this.focus();this.select();">{{.Token}}</textarea>
+			<h2>
+				The token will expire in &lt; 1 hour.
+			</h2>
+		</div>
 	</div>
-  </div>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="/static/js/bootstrap.min.js"></script>
-  </body>
+</body>
 </html>
 `
