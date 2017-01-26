c6e18579424b5a0a958892365c6a80fdf2b3e31c
Kevin Lyda <kevin@ie.suberic.net>
2017-01-26 10:01:39 +0000

    Add a script that can be used as a pre-commit.

---
+++ build.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+# This can be used as a pre-commit script.  Just run
+#   cp build.sh .git/hooks/pre-commit
+# and it will run before each commit.
+
+go install -v ./cmd/cashier ./cmd/cashierd
+go list ./... |grep -v vendor/ |xargs go test
+gofmt -d $(find * -type f -name '*.go' -not -path 'vendor/*')
+go list ./... |grep -v vendor/ |xargs go vet
+if ! type -f golint > /dev/null; then
+  go get -u github.com/golang/lint/golint
+fi
+go list ./... |grep -v vendor/ |xargs -L1 golint -set_exit_status
