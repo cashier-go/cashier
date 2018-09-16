8a9cd6cd46d39c26e87db7f800bbbfe0f4c7d16e
Kevin Lyda <kevin@ie.suberic.net>
2018-09-16 13:13:56 +0100

    Update for gitlab 11.1+
    Versions beyond 11.1 (and possibly a few releases before) use a
    different method for delivering tokens.  They also have disabled
    version 3 of the api.
    
    These changes address that and add a debugging mode for the server
    that make it easier to debug issues like this in the future.

--- server/auth/gitlab/gitlab.go
+++ server/auth/gitlab/gitlab.go
@@ -1,14 +1,18 @@
 package gitlab
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"strconv"
 
 	"github.com/nsheridan/cashier/server/config"
 	"github.com/nsheridan/cashier/server/metrics"
 
-	gitlabapi "github.com/xanzy/go-gitlab"
 	"golang.org/x/oauth2"
 )
 
@@ -20,14 +24,98 @@ const (
 // Gitlab account.
 type Config struct {
 	config    *oauth2.Config
-	baseurl   string
 	group     string
 	whitelist map[string]bool
 	allusers  bool
+	apiurl    string
+	log       bool
+}
+
+// Note on Gitlab REST API calls.  We don't parse errors because it's
+// kind of a pain:
+// https://gitlab.com/help/api/README.md#data-validation-and-error-reporting
+// The two v4 api calls used are /user and /groups/:group/members/:uid
+// https://gitlab.com/help/api/users.md#for-normal-users-1
+// https://gitlab.com/help/api/members.md#get-a-member-of-a-group-or-project
+type serviceUser struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+type serviceGroupMember struct {
+	ID          int    `json:"id"`
+	State       string `json:"state"`
+	AccessLevel int    `json:"access_level"`
+}
+
+func (c *Config) logMsg(message string) {
+	if c.log {
+		log.Print(message)
+	}
+}
+
+// A new oauth2 http client.
+func (c *Config) newClient(token *oauth2.Token) *http.Client {
+	return c.config.Client(oauth2.NoContext, token)
+}
+
+// Gets info on the current user.
+func (c *Config) getUser(token *oauth2.Token) *serviceUser {
+	client := c.newClient(token)
+	url := c.apiurl + "user"
+	resp, err := client.Get(url)
+	if err != nil {
+		return nil
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		if c.log {
+			var body bytes.Buffer
+			io.Copy(&body, resp.Body)
+			log.Printf("Gitlab error(http: %d) getting user: '%s'",
+				resp.StatusCode, body.String())
+			return nil
+		}
+	}
+	var user serviceUser
+	if err := json.NewDecoder(resp.Body).Decode(&user); err != nil {
+		return nil
+	}
+	return &user
+}
+
+// Gets current user group membership info.
+func (c *Config) checkGroupMembership(token *oauth2.Token, uid int, group string) bool {
+	client := c.newClient(token)
+	log.Printf("Checking group membership...")
+	url := fmt.Sprintf("%sgroups/%s/members/%d", c.apiurl, group, uid)
+	resp, err := client.Get(url)
+	if err != nil {
+		c.logMsg(fmt.Sprintf("Failed to get groups: %s", err))
+		return false
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		if c.log {
+			var body bytes.Buffer
+			io.Copy(&body, resp.Body)
+			log.Printf("Gitlab error(http: %d) getting user membership: '%s'",
+				resp.StatusCode, body.String())
+			return false
+		}
+	}
+	var m serviceGroupMember
+	if err := json.NewDecoder(resp.Body).Decode(&m); err != nil {
+		c.logMsg(fmt.Sprintf("Failed to parse groups: %s", err))
+		return false
+	}
+	return m.ID == uid
 }
 
 // New creates a new Gitlab provider from a configuration.
 func New(c *config.Auth) (*Config, error) {
+	logOpt, _ := strconv.ParseBool(c.ProviderOpts["log"])
 	uw := make(map[string]bool)
 	for _, u := range c.UsersWhitelist {
 		uw[u] = true
@@ -47,6 +135,7 @@ func New(c *config.Auth) (*Config, error) {
 			return nil, errors.New("gitlab_opts if allusers is set, siteurl must be set")
 		}
 	}
+	// TODO: Should make sure siteURL is just the host bit.
 	oauth2.RegisterBrokenAuthHeaderProvider(siteURL)
 
 	return &Config{
@@ -65,7 +154,8 @@ func New(c *config.Auth) (*Config, error) {
 		group:     c.ProviderOpts["group"],
 		whitelist: uw,
 		allusers:  allUsers,
-		baseurl:   siteURL + "api/v3/",
+		apiurl:    siteURL + "api/v4/",
+		log:       logOpt,
 	}, nil
 }
 
@@ -85,33 +175,28 @@ func (c *Config) Valid(token *oauth2.Token) bool {
 		metrics.M.AuthValid.WithLabelValues("gitlab").Inc()
 		return true
 	}
+	u := c.getUser(token)
+	if u == nil {
+		return false
+	}
 	if len(c.whitelist) > 0 && !c.whitelist[c.Username(token)] {
-		log.Printf("Auth fail (not in whitelist)")
+		c.logMsg("Auth fail (not in whitelist)")
 		return false
 	}
 	if c.group == "" {
 		// There's no group and token is valid.  Can only reach
 		// here if user whitelist is set and user is in whitelist.
-		log.Printf("Auth success (no groups specified in server config)")
+		c.logMsg("Auth success (no groups specified in server config)")
 		metrics.M.AuthValid.WithLabelValues("gitlab").Inc()
 		return true
 	}
-	client := gitlabapi.NewOAuthClient(nil, token.AccessToken)
-	client.SetBaseURL(c.baseurl)
-	groups, _, err := client.Groups.SearchGroup(c.group)
-	if err != nil {
-		log.Printf("Auth failure (error fetching groups: %s)", err)
+	if !c.checkGroupMembership(token, u.ID, c.group) {
+		c.logMsg("Auth failure (not in allowed group)")
 		return false
 	}
-	for _, g := range groups {
-		if g.Path == c.group {
-			metrics.M.AuthValid.WithLabelValues("gitlab").Inc()
-			log.Printf("Auth success (in allowed group)")
-			return true
-		}
-	}
-	log.Printf("Auth failure (not in allowed groups)")
-	return false
+	metrics.M.AuthValid.WithLabelValues("gitlab").Inc()
+	c.logMsg("Auth success (in allowed group)")
+	return true
 }
 
 // Revoke is a no-op revoke method. Gitlab doesn't allow token
@@ -137,10 +222,8 @@ func (c *Config) Exchange(code string) (*oauth2.Token, error) {
 
 // Username retrieves the username of the Gitlab user.
 func (c *Config) Username(token *oauth2.Token) string {
-	client := gitlabapi.NewOAuthClient(nil, token.AccessToken)
-	client.SetBaseURL(c.baseurl)
-	u, _, err := client.Users.CurrentUser()
-	if err != nil {
+	u := c.getUser(token)
+	if u == nil {
 		return ""
 	}
 	return u.Username
